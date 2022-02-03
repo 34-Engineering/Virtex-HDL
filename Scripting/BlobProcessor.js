9d906af7b879b3e32a620259f8cde8ecc6f8f3e9
Liam Snow <liamsnow03@gmail.com>
2022-02-02 21:54:30 -0500

    BlobProcessor.js Working!

--- Scripting/BlobProcessor.js
+++ Scripting/BlobProcessor.js
@@ -5,72 +5,82 @@ const png = drawing(require('pngjs').PNG);
 let blobs = [];
 let blobPointer = 0;
 let joined = 255; //the index of of the blob is last joined
+let frameBuffer = [...Array(1000)].map(e => Array(1000));
 
 fs.createReadStream('images/2019_Noise2.png')
     .pipe(new png())
-    .on('parsed', function() {
+    .on('parsed', function () {
 
-    //Process all Pixels
-    for (let y = 0; y < this.height; y++) {
-        for (let x = 0; x < this.width; x++) {
-            const idx = (this.width * y + x) << 2;
-            const value = (this.data[idx] + this.data[idx+1] + this.data[idx+2]) / 3; 
+        //Process all Pixels
+        for (let y = 0; y < this.height; y++) {
+            for (let x = 0; x < this.width; x++) {
+                const idx = (this.width * y + x) << 2;
+                const value = (this.data[idx] + this.data[idx + 1] + this.data[idx + 2]) / 3;
+                const threshold = value > 128;
 
-            if (value > 128) {
-                processPixel({x, y});
+                if (threshold) {
+                    processPixel({
+                        x,
+                        y
+                    });
+                }
+
+                frameBuffer[x][y] = threshold;
             }
         }
-    }
 
-    console.log("num:"+blobs.length);
+        console.log("num:" + blobs.length);
 
-    //Draw Blobs
-    for (let i = 0; i < blobs.length; i++) {
-        if (blobs[i].valid) {
-            this.drawRect(
-                blobs[i].boundTopLeft.x,
-                blobs[i].boundTopLeft.y,
-                blobs[i].boundBottomRight.x - blobs[i].boundTopLeft.x,
-                blobs[i].boundBottomRight.y - blobs[i].boundTopLeft.y,
-                this.colors.red(255)
-            );
+        //Draw Blobs
+        for (let i = 0; i < blobs.length; i++) {
+            const area = (blobs[i].boundBottomRight.x - blobs[i].boundTopLeft.x) * (blobs[i].boundBottomRight.y - blobs[i].boundTopLeft.y);
+            if (blobs[i].valid && area > 1000) {
+                this.drawRect(
+                    blobs[i].boundTopLeft.x,
+                    blobs[i].boundTopLeft.y,
+                    blobs[i].boundBottomRight.x - blobs[i].boundTopLeft.x,
+                    blobs[i].boundBottomRight.y - blobs[i].boundTopLeft.y,
+                    this.colors.red(255)
+                );
 
-            // this.drawLine(
-            //     blobs[i].cornerTopLeft.x,
-            //     blobs[i].cornerTopLeft.y,
-            //     blobs[i].cornerTopRight.x,
-            //     blobs[i].cornerTopRight.y,
-            //     this.colors.green(255)
-            // );
+                const stats = getStats(blobs[i]);
 
-            // this.drawLine(
-            //     blobs[i].cornerTopRight.x,
-            //     blobs[i].cornerTopRight.y,
-            //     blobs[i].cornerBottomRight.x,
-            //     blobs[i].cornerBottomRight.y,
-            //     this.colors.green(255)
-            // );
+                this.drawLine(
+                    stats.cornerTopLeft.x,
+                    stats.cornerTopLeft.y,
+                    stats.cornerTopRight.x,
+                    stats.cornerTopRight.y,
+                    this.colors.green(255)
+                );
 
-            // this.drawLine(
-            //     blobs[i].cornerBottomRight.x,
-            //     blobs[i].cornerBottomRight.y,
-            //     blobs[i].cornerBottomLeft.x,
-            //     blobs[i].cornerBottomLeft.y,
-            //     this.colors.green(255)
-            // );
+                this.drawLine(
+                    stats.cornerTopRight.x,
+                    stats.cornerTopRight.y,
+                    stats.cornerBottomRight.x,
+                    stats.cornerBottomRight.y,
+                    this.colors.green(255)
+                );
 
-            // this.drawLine(
-            //     blobs[i].cornerBottomLeft.x,
-            //     blobs[i].cornerBottomLeft.y,
-            //     blobs[i].cornerTopLeft.x,
-            //     blobs[i].cornerTopLeft.y,
-            //     this.colors.green(255)
-            // );
+                this.drawLine(
+                    stats.cornerBottomRight.x,
+                    stats.cornerBottomRight.y,
+                    stats.cornerBottomLeft.x,
+                    stats.cornerBottomLeft.y,
+                    this.colors.green(255)
+                );
+
+                this.drawLine(
+                    stats.cornerBottomLeft.x,
+                    stats.cornerBottomLeft.y,
+                    stats.cornerTopLeft.x,
+                    stats.cornerTopLeft.y,
+                    this.colors.green(255)
+                );
+            }
         }
-    }
 
-    this.pack().pipe(fs.createWriteStream('out.png'));
-});
+        this.pack().pipe(fs.createWriteStream('out.png'));
+    });
 
 //Process Pixel
 function processPixel(pos) {
@@ -80,7 +90,7 @@ function processPixel(pos) {
     for (let i = 0; i < blobs.length; i++) {
         //if point inside blob bounding box
         if (blobs[i].valid &&
-            pos.x + 2 > blobs[i].boundTopLeft.x && 
+            pos.x + 2 > blobs[i].boundTopLeft.x &&
             pos.x - 2 < blobs[i].boundBottomRight.x &&
             pos.y + 2 > blobs[i].boundTopLeft.y &&
             pos.y - 2 < blobs[i].boundBottomRight.y) {
@@ -94,24 +104,6 @@ function processPixel(pos) {
                 blobs[i].boundTopLeft.y = min(blobs[i].boundTopLeft.y, blobs[joined].boundTopLeft.y);
                 blobs[i].boundBottomRight.x = max(blobs[i].boundBottomRight.x, blobs[joined].boundBottomRight.x);
                 blobs[i].boundBottomRight.y = max(blobs[i].boundBottomRight.y, blobs[joined].boundBottomRight.y);
-                                    
-                //make new corners
-                // if (blobs[joined].cornerTopLeft.x+blobs[joined].cornerTopLeft.y < blobs[i].cornerTopLeft.x+blobs[i].cornerTopLeft.y) {
-                //     blobs[i].cornerTopLeft.x = blobs[joined].cornerTopLeft.x;
-                //     blobs[i].cornerTopLeft.y = blobs[joined].cornerTopLeft.y;
-                // }
-                // else if (blobs[joined].cornerTopRight.x-blobs[joined].cornerTopRight.y > blobs[i].cornerTopRight.x-blobs[i].cornerTopRight.y) {
-                //     blobs[i].cornerTopRight.x = blobs[joined].cornerTopRight.x;
-                //     blobs[i].cornerTopRight.y = blobs[joined].cornerTopRight.y;
-                // }
-                // else if (blobs[joined].cornerBottomRight.x+blobs[joined].cornerBottomRight.y > blobs[i].cornerBottomRight.x+blobs[i].cornerBottomRight.y) {
-                //     blobs[i].cornerBottomRight.x = blobs[joined].cornerBottomRight.x;
-                //     blobs[i].cornerBottomRight.y = blobs[joined].cornerBottomRight.y;
-                // }
-                // else if (blobs[joined].cornerBottomLeft.x-blobs[joined].cornerBottomLeft.y < blobs[i].cornerBottomLeft.x-blobs[i].cornerBottomLeft.y) {
-                //     blobs[i].cornerBottomLeft.x = blobs[joined].cornerBottomLeft.x;
-                //     blobs[i].cornerBottomLeft.y = blobs[joined].cornerBottomLeft.y;
-                // }
             }
 
             joined = i;
@@ -125,42 +117,68 @@ function processPixel(pos) {
                 blobs[i].boundTopLeft.y = blobs[i].boundTopLeft.y - 1;
             else if (pos.y + 1 > blobs[i].boundBottomRight.y)
                 blobs[i].boundBottomRight.y = blobs[i].boundBottomRight.y + 1;
-
-            //move corner vertexes
-            // if (pos.x + pos.y < blobs[i].cornerTopLeft.x + blobs[i].cornerTopLeft.y) {
-            //     blobs[i].cornerTopLeft.x = pos.x;
-            //     blobs[i].cornerTopLeft.y = pos.y;
-            // }
-            // else if (pos.x - pos.y > blobs[i].cornerTopRight.x - blobs[i].cornerTopRight.y) {
-            //     blobs[i].cornerTopRight.x = pos.x;
-            //     blobs[i].cornerTopRight.y = pos.y;
-            // }
-            // else if (pos.x + pos.y > blobs[i].cornerBottomRight.x + blobs[i].cornerBottomRight.y) {
-            //     blobs[i].cornerBottomRight.x = pos.x;
-            //     blobs[i].cornerBottomRight.y = pos.y;
-            // }
-            // else if (pos.x - pos.y < blobs[i].cornerBottomLeft.x - blobs[i].cornerBottomLeft.y) {
-            //     blobs[i].cornerBottomLeft.x = pos.x;
-            //     blobs[i].cornerBottomLeft.y = pos.y;
-            // }
         }
     }
 
     //didnt join any blobs --> make new blob
     if (joined == 255) {
         blobs[blobPointer] = {
-            boundTopLeft: {x:pos.x, y:pos.y},
-            boundBottomRight: {x:pos.x + 1, y:pos.y + 1},
-            // cornerTopLeft: {x:pos.x, y:pos.y},
-            // cornerTopRight: {x:pos.x, y:pos.y}, //x+1?
-            // cornerBottomRight: {x:pos.x, y:pos.y}, //x+1,y+1?
-            // cornerBottomLeft: {x:pos.x, y:pos.y}, //y+1?
+            boundTopLeft: {
+                x: pos.x,
+                y: pos.y
+            },
+            boundBottomRight: {
+                x: pos.x + 1,
+                y: pos.y + 1
+            },
             valid: 1,
             reserved: 0
         };
         blobPointer = blobPointer + 1;
         fixBlobPointer();
     }
+}
+
+//Get Stats
+function getStats(blob) {
+    const middleX = (blob.boundTopLeft.x + blob.boundBottomRight.x) / 2;
+    const middleY = (blob.boundTopLeft.y + blob.boundBottomRight.y) / 2;
+
+    let cornerTopLeft = {x: middleX, y: middleY},
+        cornerTopRight = {x: middleX, y: middleY},
+        cornerBottomRight = {x: middleX, y: middleY},
+        cornerBottomLeft = {x: middleX, y: middleY};
+
+    for (let y = blob.boundTopLeft.y; y < blob.boundBottomRight.y; y++) {
+        for (let x = blob.boundTopLeft.x; x < blob.boundBottomRight.x; x++) {
+            if (frameBuffer[x][y]) {
+                //move corner vertexes
+                if (x + y < cornerTopLeft.x + cornerTopLeft.y) {
+                    cornerTopLeft.x = x;
+                    cornerTopLeft.y = y;
+                }
+                else if (x - y > cornerTopRight.x - cornerTopRight.y) {
+                    cornerTopRight.x = x;
+                    cornerTopRight.y = y;
+                }
+                else if (x + y > cornerBottomRight.x + cornerBottomRight.y) {
+                    cornerBottomRight.x = x;
+                    cornerBottomRight.y = y;
+                }
+                else if (x - y < cornerBottomLeft.x - cornerBottomLeft.y) {
+                    cornerBottomLeft.x = x;
+                    cornerBottomLeft.y = y;
+                }
+            }
+        }
+    }
+
+    return {
+        cornerTopLeft,
+        cornerTopRight,
+        cornerBottomRight,
+        cornerBottomLeft
+    };
 }
 
 //Fix Blob Pointer
@@ -171,7 +189,7 @@ function fixBlobPointer() {
     //     if (blobPointer > blobs.length - 1) {
     //         blobPointer = 0;
     //     }
-        
+
     //     for (let i = 0; i < blobs.length; i++) {
     //         if (!blobs[i].valid) {
     //             blobPointer = i;
