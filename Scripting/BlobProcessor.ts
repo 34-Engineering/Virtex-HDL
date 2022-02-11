ebbacba6b8553b13da7c5fe763a7bee041f392b6
Liam Snow <liamsnow03@gmail.com>
2022-02-11 11:38:03 -0500

    RLE Working!

--- Scripting/BlobProcessor.ts
+++ Scripting/BlobProcessor.ts
@@ -11,9 +11,10 @@ const MAX_BLOBS = 1000;
 const NULL_BLOB_ID = MAX_BLOBS;
 const IMAGE_WIDTH = 640;
 const IMAGE_HEIGHT = 480;
-const IMAGE_PATH = 'images/Test2.png';
+const IMAGE_PATH = 'images/2019_Noise2.png';
+const NULL_RUN_START = IMAGE_WIDTH;
 const DRAW_BLOB_COLOR = true;
-const DRAW_BOUND = true;
+const DRAW_BOUND = false;
 const DRAW_QUAD = false;
 
 //Interfaces
@@ -40,7 +41,6 @@ interface Run {
 
 //Variables
 let frameBuffer: boolean[][] = Array.from(Array(IMAGE_HEIGHT), () => Array(IMAGE_WIDTH).fill(0));
-let blobColorBuffer: number[][] = Array.from(Array(IMAGE_WIDTH), () => Array(IMAGE_HEIGHT).fill(0)); //for coloring blobs (only for script version)
 let blobs: BlobData[] = Array(MAX_BLOBS).fill({
     boundTopLeft: {x:0, y:0},
     boundBottomRight: {x:0, y:0},
@@ -53,6 +53,9 @@ let blobs: BlobData[] = Array(MAX_BLOBS).fill({
     valid: false
 });
 let blobIndex = 0;
+let runBuffer: Run[] = Array(IMAGE_WIDTH / 8).fill({ start: NULL_RUN_START, end: 0, blobID: NULL_BLOB_ID });
+let lastRunBuffer: Run[] = Array(IMAGE_WIDTH / 8).fill({ start: NULL_RUN_START, end: 0, blobID: NULL_BLOB_ID });
+let blobColorBuffer: Run[][] = Array.from(Array(IMAGE_HEIGHT), () => Array(IMAGE_WIDTH / 8).fill({ start: NULL_RUN_START, end: 0, blobID: NULL_BLOB_ID })); //for coloring blobs (only for script version)
 
 //Read Image
 fs.createReadStream(IMAGE_PATH)
@@ -64,28 +67,28 @@ fs.createReadStream(IMAGE_PATH)
                 const idx = (IMAGE_WIDTH * y + x) << 2;
                 //@ts-ignore
                 const value = (this.data[idx] + this.data[idx + 1] + this.data[idx + 2]) / 3;
-                const threshold = value > 128;
-                frameBuffer[y][x] = threshold;
+                frameBuffer[y][x] = value > 128;
             }
 
             processLine(y);
         }
 
-        //Log Data
-        console.log("blob count: ", blobIndex - 1);
-
         //Draw Blob Color
         if (DRAW_BLOB_COLOR) {
-            for (let y = 0; y < IMAGE_HEIGHT; y++) {
-                for (let x = 0; x < IMAGE_WIDTH; x++) {
-                    if (blobColorBuffer[x][y] !== MAX_BLOBS) {
-                        const idx = (IMAGE_WIDTH * y + x) << 2;
-                        const blobIndex = getRealBlobID(blobColorBuffer[x][y]);
-                        // const blobIndex = blobColorBuffer[x][y];
-                        //@ts-ignore
-                        this.data[idx] = Math.sin(blobIndex * 50) * 255;
-                        //@ts-ignore
-                        this.data[idx + 1] = Math.sin(blobIndex * 100) * 255;
+            for (let y = 0; y < blobColorBuffer.length; y++) {
+                for (let i = 0; i < blobColorBuffer[y].length; i++) {
+                    const run = blobColorBuffer[y][i];
+                    if (run.start !== NULL_RUN_START) {
+                        for (let x = run.start; x <= run.end; x++) {
+                            const idx = (IMAGE_WIDTH * y + x) << 2;
+                            const realBlobID = getRealBlobID(run.blobID);
+                            //@ts-ignore
+                            this.data[idx] = Math.sin(realBlobID * 50) * 200 + 55;
+                            //@ts-ignore
+                            this.data[idx + 1] = Math.sin(realBlobID * 100) * 200 + 55;
+                            //@ts-ignore
+                            this.data[idx + 2] = Math.sin(realBlobID * 200) * 200 + 55;
+                        }
                     }
                 }
             }
@@ -151,28 +154,42 @@ fs.createReadStream(IMAGE_PATH)
     });
 
 //Process Line
-let lastRunBuffer: Run[] = Array(IMAGE_WIDTH / 8).fill({ start: 0, end: 0, blobID: NULL_BLOB_ID });
-let runBuffer: Run[] = Array(IMAGE_WIDTH / 8).fill({ start: 0, end: 0, blobID: NULL_BLOB_ID });
+let inRun: boolean = false;
+let runBufferIndex: number = 0;
 function processLine(line: number) {
     lastRunBuffer = runBuffer;
+    runBuffer = Array(IMAGE_WIDTH / 8).fill({ start: NULL_RUN_START, end: 0, blobID: NULL_BLOB_ID });
+    runBufferIndex = 0;
 
     //encode line into runs
-    let inRun: boolean = false;
-    let start: number = 0, runBufferIndex: number = 0;
+    let start: number = 0;
     for (let x = 0; x < IMAGE_WIDTH; x++) {
         if (frameBuffer[line][x]) {
             if (!inRun) {
                 inRun = true;
                 start = x;
             }
+            
+            if (x == IMAGE_WIDTH-1) {
+                endRun(line, start, x);
+            }
         }
         else if (inRun) {
-            inRun = false;
-            runBuffer[runBufferIndex] = { start: start, end: x-1, blobID: NULL_BLOB_ID };
-            processRun(line, runBuffer[runBufferIndex]);
-            runBufferIndex++;
+            endRun(line, start, x-1);
         }
     }
+
+    //just for scripting
+    blobColorBuffer[line] = runBuffer;
+}
+
+function endRun(line: number, start: number, end: number) {
+    inRun = false;
+    runBuffer[runBufferIndex] = {
+        start, end,
+        blobID: processRun(line, { start, end, blobID: NULL_BLOB_ID })
+    };
+    runBufferIndex++;
 }
 
 //Process Pixel & Return BlobID
@@ -180,17 +197,21 @@ let masterBlobID = NULL_BLOB_ID;
 function processRun(line: number, run: Run): number {
     masterBlobID = NULL_BLOB_ID;
 
-    // for (let i = 0; i < lastRunBuffer.length; i++) {
-    //     //widen run to join other runs its touching but not overlapping
-    //     let realStart = run.start - (run.start == 0 ?0:1);
-    //     let realEnd = run.end + (run.end == IMAGE_WIDTH-1 ?0:1);
+    for (let i = 0; i < lastRunBuffer.length; i++) {
+        if (lastRunBuffer[i].blobID !== NULL_BLOB_ID) {
+            //widen run to join other runs its touching but not overlapping
+            let realStart = run.start - (run.start == 0 ?0:1);
+            let realEnd = run.end + 1;
 
-    //     //check if lines overlap
-    //     if ((lastRunBuffer[i].start >= realStart && lastRunBuffer[i].start <= realEnd) ||
-    //         (lastRunBuffer[i].end   >= realStart && lastRunBuffer[i].end   <= realEnd)) {
-    //         joinTopRun(line, run, lastRunBuffer[i]);
-    //     }
-    // }
+            //check if lines overlap
+            if ((lastRunBuffer[i].start >= realStart && lastRunBuffer[i].start <= realEnd) ||
+                (lastRunBuffer[i].end   >= realStart && lastRunBuffer[i].end   <= realEnd) ||
+                (lastRunBuffer[i].start <  realStart && lastRunBuffer[i].end   > realEnd)) {
+                
+                joinTopRun(line, run, lastRunBuffer[i]);
+            }
+        }
+    }
 
     //add this pixel to blob if we have a valid blob to join
     if (masterBlobID !== NULL_BLOB_ID) {
@@ -223,21 +244,21 @@ function joinTopRun(line: number, run: Run, topRun: Run) {
 
         //found another valid blob => merge with master
         else if (getRealBlobID(topRun.blobID) !== masterBlobID) {
-            // //merge slave into master
-            // blobs[masterBlobID] = mergeBlobs(blobs[topRun.blobID], blobs[masterBlobID]);
+            //merge slave into master
+            blobs[masterBlobID] = mergeBlobs(blobs[topRun.blobID], blobs[masterBlobID]);
 
-            // //mark slave as pointer to master
-            // blobs[topRun.blobID] = {
-            //     boundTopLeft: {x:0, y:0},
-            //     boundBottomRight: {x:0, y:0},
-            //     quadTopLeft: {x:0, y:0},
-            //     quadTopRight: {x:0, y:0},
-            //     quadBottomLeft: {x:0, y:0},
-            //     quadBottomRight: {x:0, y:0},
-            //     area: 0,
-            //     pointer: masterBlobID,
-            //     valid: false
-            // };
+            //mark slave as pointer to master
+            blobs[topRun.blobID] = {
+                boundTopLeft: {x:0, y:0},
+                boundBottomRight: {x:0, y:0},
+                quadTopLeft: {x:0, y:0},
+                quadTopRight: {x:0, y:0},
+                quadBottomLeft: {x:0, y:0},
+                quadBottomRight: {x:0, y:0},
+                area: 0,
+                pointer: masterBlobID,
+                valid: false
+            };
         }
     }
 }
@@ -280,15 +301,14 @@ function mergeQuadBottomLeft(a: Vector, b: Vector): Vector {
 
 //Run to Blob
 function runToBlob(line: number, run: Run): BlobData {
-    const width = (run.start-run.end)+1
-    ;
+    const width = (run.end-run.start)+1;
     return {
-        boundTopLeft:     {x:run.start      , y:line      },
-        boundBottomRight: {x:run.start+width, y:line+width},
-        quadTopLeft:      {x:run.start      , y:line      },
-        quadTopRight:     {x:run.start+width, y:line      },
-        quadBottomLeft:   {x:run.start      , y:line+width},
-        quadBottomRight:  {x:run.start+width, y:line+width},
+        boundTopLeft:     {x:run.start      , y:line  },
+        boundBottomRight: {x:run.start+width, y:line+1},
+        quadTopLeft:      {x:run.start      , y:line  },
+        quadTopRight:     {x:run.start+width, y:line  },
+        quadBottomLeft:   {x:run.start      , y:line+1},
+        quadBottomRight:  {x:run.start+width, y:line+1},
         area: width,
         pointer: 0,
         valid: true
--- Scripting/README.md
+++ Scripting/README.md
@@ -2,6 +2,8 @@
 
 This folder is for scripting the functionality of the Blob Processor before writing it in System Verilog (BlobProcessor.sv)
 
+This code is focuses on being easy to translate to System Verilog and has no emphasis on Typescript performance.
+
 An image is read from the `images` folder, processed, and then output to `out.png`.
 
 ## Running
